c1b6cded4f726ba27eb20812cef91b769ed91b42
BoseVJeff <jeffbvic003@gmail.com>
2025-10-08 14:08:51 +0530

    Updated shared preferences demo
    Updates visual design, edits labels, and adds a bar to show if no restorable data is available.

--- lib/pages/shared_preferences_page.dart
+++ lib/pages/shared_preferences_page.dart
@@ -45,43 +45,54 @@ class _SpFormState extends State<SpForm> {
       child: ListView(
         padding: const EdgeInsets.all(8.0),
         children: [
-          TextFormField(
-            controller: _itemController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Item Name",
+          Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(left: 24),
+            child: TextFormField(
+              controller: _itemController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Item Name",
+                icon: Icon(Icons.badge),
+              ),
+              textInputAction: TextInputAction.next,
+              onSaved: (newValue) async {
+                print(newValue);
+                if (newValue != null) {
+                  await _preferencesAsync.setString("item", newValue);
+                } else {
+                  await _preferencesAsync.remove("item");
+                }
+              },
             ),
-            textInputAction: TextInputAction.next,
-            onSaved: (newValue) async {
-              print(newValue);
-              if (newValue != null) {
-                await _preferencesAsync.setString("item", newValue);
-              } else {
-                await _preferencesAsync.remove("item");
-              }
-            },
           ),
-          TextFormField(
-            controller: _countController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Item Count",
+          Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(left: 24),
+            child: TextFormField(
+              controller: _countController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Item Count",
+                icon: Icon(Icons.numbers),
+              ),
+
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              onSaved: (newValue) async {
+                print(newValue);
+                int? parsedValue = int.tryParse(newValue ?? "NaN");
+                if (parsedValue != null) {
+                  await _preferencesAsync.setInt("count", parsedValue);
+                } else {
+                  await _preferencesAsync.remove("count");
+                }
+              },
             ),
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.next,
-            onSaved: (newValue) async {
-              print(newValue);
-              int? parsedValue = int.tryParse(newValue ?? "NaN");
-              if (parsedValue != null) {
-                await _preferencesAsync.setInt("count", parsedValue);
-              } else {
-                await _preferencesAsync.remove("count");
-              }
-            },
           ),
           FormField<bool>(
             builder: (FormFieldState field) => CheckboxListTile(
+              // side: BorderSide(color: Colors.black),
+              controlAffinity: ListTileControlAffinity.leading,
               title: Text("Star"),
               value: isChecked,
               onChanged: (bool? value) {
@@ -102,58 +113,91 @@ class _SpFormState extends State<SpForm> {
               }
             },
           ),
-          Builder(
-            builder: (context) {
-              return FilledButton.icon(
-                onPressed: () {
-                  Form.of(context).save();
-                },
-                label: Text("Save"),
-                icon: Icon(Icons.save),
-              );
-            },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Builder(
+              builder: (context) {
+                return FilledButton.icon(
+                  onPressed: () {
+                    Form.of(context).save();
+                  },
+                  label: Text(
+                    "Save to Shared Preferences",
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                  ),
+                  icon: Icon(Icons.save),
+                );
+              },
+            ),
           ),
-          Builder(
-            builder: (context) {
-              return FilledButton.icon(
-                onPressed: () async {
-                  String? item = await _preferencesAsync.getString("item");
-                  int? count = await _preferencesAsync.getInt("count");
-                  bool? star = await _preferencesAsync.getBool("star");
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Builder(
+              builder: (context) {
+                return FilledButton.icon(
+                  onPressed: () async {
+                    String? item = await _preferencesAsync.getString("item");
+                    int? count = await _preferencesAsync.getInt("count");
+                    bool? star = await _preferencesAsync.getBool("star");
 
-                  if (item != null) {
-                    _itemController.value = _itemController.value.copyWith(
-                      text: item,
-                    );
-                  }
-                  if (count != null) {
-                    _countController.value = _countController.value.copyWith(
-                      text: count.toRadixString(10),
-                    );
-                  }
-                  if (star != null) {
-                    setState(() {
-                      isChecked = star;
-                    });
-                  }
-                },
-                label: Text("Restore"),
-                icon: Icon(Icons.restore),
-              );
-            },
+                    if (item == null && count == null && star == null) {
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("No data to restore!"),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      }
+                      return;
+                    }
+
+                    if (item != null) {
+                      _itemController.value = _itemController.value.copyWith(
+                        text: item,
+                      );
+                    }
+                    if (count != null) {
+                      _countController.value = _countController.value.copyWith(
+                        text: count.toRadixString(10),
+                      );
+                    }
+                    if (star != null) {
+                      setState(() {
+                        isChecked = star;
+                      });
+                    }
+                  },
+                  label: Text(
+                    "Restore from Shared Preferences",
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                  ),
+                  icon: Icon(Icons.restore),
+                );
+              },
+            ),
           ),
-          Builder(
-            builder: (context) {
-              return FilledButton.icon(
-                onPressed: () async {
-                  await _preferencesAsync.remove("item");
-                  await _preferencesAsync.remove("count");
-                  await _preferencesAsync.remove("star");
-                },
-                label: Text("Clear Store"),
-                icon: Icon(Icons.clear),
-              );
-            },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Builder(
+              builder: (context) {
+                return FilledButton.icon(
+                  onPressed: () async {
+                    await _preferencesAsync.remove("item");
+                    await _preferencesAsync.remove("count");
+                    await _preferencesAsync.remove("star");
+                  },
+                  label: Text(
+                    "Clear Shared Preferences",
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                  ),
+                  icon: Icon(Icons.clear),
+                );
+              },
+            ),
           ),
         ],
       ),
